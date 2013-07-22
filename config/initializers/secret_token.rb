f6c5015fb54d99f04a7a75cd63651d33b15e04e3
hui <hui@networking.io>
2013-07-22 13:07:09 +0800

    init CRUD.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TurbolinksOnRails4::Application.config.secret_key_base = 'eaef71f8e8e08574bbb3b5312770f05056b4777efc894622a482381d63e691b19c50841939ee184e426df210f2dfdef9a00f3a74df4ca849de478de066d82e74'
